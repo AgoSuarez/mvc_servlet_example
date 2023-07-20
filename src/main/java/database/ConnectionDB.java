e8f1befa62c3ca2e42c12d49787139cf38dc4984
AgoSuarez <agosuaort@gmail.com>
2023-07-20 16:04:01 +0100

    Improve DB connection. Reuse datasource

--- src/main/java/database/ConnectionDB.java
+++ src/main/java/database/ConnectionDB.java
@@ -1,6 +1,5 @@
 package database;
 
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -13,36 +12,47 @@ public class ConnectionDB {
     private static final String JDBC_URL = "jdbc:mysql://192.168.1.131:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "password";
-    
-    public static DataSource getDataSource(){
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        //Init Connection Pool 
-        ds.setInitialSize(50);
-        return ds;
+
+    private static BasicDataSource datasource;
+
+    public static DataSource getDataSource() {
+        //init datasource  only the first time, if exist we reuse  it
+        if (datasource == null) {
+            datasource = new BasicDataSource();
+            datasource.setUrl(JDBC_URL);
+            datasource.setUsername(JDBC_USER);
+            datasource.setPassword(JDBC_PASSWORD);
+            //Init Connection Pool 
+            datasource.setInitialSize(50);
+
+        }
+
+        return datasource;
     }
-    public static Connection getConnection() throws SQLException{
-        return  getDataSource().getConnection();
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
     }
+
     //Close resultset
-    public static void close(ResultSet rs){
+    public static void close(ResultSet rs) {
         try {
             rs.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
+
     //close statement
-    public static void close(PreparedStatement ps){
+    public static void close(PreparedStatement ps) {
         try {
             ps.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
-    public static void close(Connection con){
+
+    public static void close(Connection con) {
         try {
             con.close();
         } catch (SQLException ex) {
